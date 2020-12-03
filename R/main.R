c40f0112db237e1c2ed4f56713955f28914c2180
Amin Adibi <adibi@alumni.ubc.ca>
2020-12-03 13:55:11 -0800

    binorm now accepts means

--- R/main.R
+++ R/main.R
@@ -1,11 +1,14 @@
-binorm_pdf <- function(x, sigma) {
+binorm_pdf <- function(x, mean = c(0, 0), sigma) {
   s1 <- sqrt(sigma[1, 1])
   s2 <- sqrt(sigma[2, 2])
+  mu1 <- mean[1]
+  mu2 <- mean[2]
   rho <- sigma[1, 2] / (s1 * s2)
-  Z <- x[1] ^ 2 / sigma[1, 1] - 2 * rho * x[1] * x[2] / (s1 * s2) + x[2] ^ 2 / sigma[2, 2]
+  Z <- (x[1] - mu1) ^ 2 / RE_var_mat[1, 1] - 2 * rho * (x[1] - mu1) * (x[2] - mu2) / (s1 * s2) + (x[2] - mu2) ^ 2 / RE_var_mat[2, 2]
   densRes <- 1 / (2 * pi * s1 * s2 * sqrt(1 - rho ^ 2)) * exp(- Z / (2 * (1 - rho ^ 2)))
   return(densRes)
 }
+
 
 
 #' Faster multivariate normal density using multiple processor cores
@@ -15,7 +18,7 @@ binorm_pdf <- function(x, sigma) {
 #' @export
 fastdmvnorm <- function(x, mean = rep(0, length(x)), sigma) {
   if (length(x) == 2) {
-    result <- binorm_pdf(x, sigma)
+    result <- binorm_pdf(x, mean, sigma)
   } else {
     cores <- detectCores(logical = FALSE)
     result <- dmvnrm_arma_mc(x, mean, sigma, cores = cores)
